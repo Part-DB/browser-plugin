071f9874ff80fec8b8da400ab2ac1877d1afe803
Jan Böhmer <mail@jan-boehmer.de>
2026-05-14 22:08:31 +0200

    Fixed issues for mozilla plugin release

--- src/i18n.js
+++ src/i18n.js
@@ -5,7 +5,10 @@ function applyI18n() {
     }
     for (const el of document.querySelectorAll('[data-i18n-html]')) {
         const msg = chrome.i18n.getMessage(el.dataset.i18nHtml);
-        if (msg) el.innerHTML = msg;
+        if (msg) {
+            const doc = new DOMParser().parseFromString(msg, 'text/html');
+            el.replaceChildren(...doc.body.childNodes);
+        }
     }
     for (const el of document.querySelectorAll('[data-i18n-placeholder]')) {
         const msg = chrome.i18n.getMessage(el.dataset.i18nPlaceholder);
